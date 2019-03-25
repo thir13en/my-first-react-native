62160062957842f4e4945ed5df17a072c6c2cfdf
Santiago Lopez <santiago.lopez@i2cat.net>
2019-03-25 10:06:52 +0100

    added employee creation in store and component

--- src/components/applications/EmployeeCreate/EmployeeCreate.js
+++ src/components/applications/EmployeeCreate/EmployeeCreate.js
@@ -1,25 +1,29 @@
+// react
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
 
-import Card from '../../shared/Card';
-import CardSection from '../../shared/CardSection';
-import Input from '../../shared/Input';
-import Button from '../../shared/Button';
+import { employeeUpdate } from '../../../store/actions';
+import { Card, CardSection, Input, Button } from '../../shared';
 
 class EmployeeCreate extends Component {
   render() {
+    const { name, phone, EmployeeUpdate } = this.props;
     return (
       <Card>
         <CardSection>
           <Input
             label="Name"
             placeholder="John"
+            value={name}
+            onChangeText={value => EmployeeUpdate({ prop: 'name', value })}
           />
         </CardSection>
         <CardSection>
           <Input
             label="Phone"
             placeholder="555-555-5555"
+            value={phone}
+            onChangeText={value => EmployeeUpdate({ prop: 'phone', value })}
           />
         </CardSection>
         <CardSection>
@@ -30,4 +34,9 @@ class EmployeeCreate extends Component {
   }
 }
 
-export default EmployeeCreate;
+const mapStateToProps = (state) => {
+  const { name, phone, shift } = state.employeeForm;
+  return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, { EmployeeUpdate: employeeUpdate })(EmployeeCreate);
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux';
 import LibraryReducer from './LibraryReducer';
 import selectedLibrary from './SelectionReducer';
 import AuthReducer from './AuthReducer';
+import EmployeeFormReducer from './EmployeeFormReducer';
 
 export default combineReducers({
   libraries: LibraryReducer,
   selectedLibrary,
-  auth: AuthReducer
+  auth: AuthReducer,
+  employeeForm: EmployeeFormReducer
 });
